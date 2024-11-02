f8fc4cfc82240de8861e2d3c931ef18b24fedf89
S4vi0r <gbp17@outlook.com>
2024-11-01 21:17:34 -0500

    first commit

---
+++ src/main/java/com/sanmarcos/promecal/PromecalApplication.java
@@ -0,0 +1,13 @@
+package com.sanmarcos.promecal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromecalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PromecalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sanmarcos/promecal/PromecalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanmarcos.promecal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromecalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
