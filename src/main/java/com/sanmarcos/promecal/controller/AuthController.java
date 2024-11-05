8abb9390c70f96f386c8e272f89239338208fba0
S4vi0r <gbp17@outlook.com>
2024-11-05 16:49:38 -0500

    Add CORS configuration and update AuthController imports; enable debug logging for Spring Security

--- src/main/java/com/sanmarcos/promecal/controller/AuthController.java
+++ src/main/java/com/sanmarcos/promecal/controller/AuthController.java
@@ -6,10 +6,7 @@ import com.sanmarcos.promecal.model.dto.RegisterRequest;
 import com.sanmarcos.promecal.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
